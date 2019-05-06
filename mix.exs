b226dfd55a8e392bd8f75b3823f1e1cc0e5e82fc
Lee Sylvester <lee.sylvester@gmail.com>
2019-05-06 18:04:45 +0100

    Lower required Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Xirsys.XTurn.SimpleAuth.MixProject do
     [
       app: :xturn_simple_auth,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
